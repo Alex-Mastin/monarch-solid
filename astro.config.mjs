a6b41cb0f775ccf16d4f91fb629128c9d7379388
Alex Mastin <mastin.alexander@gmail.com>
2023-03-17 08:54:37 -0500

    chore: tweaked github actions config

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,26 +28,6 @@ export default defineConfig({
         '@': resolve(__dirname, './src')
       },
       conditions: ['development', 'browser']
-    },
-    test: {
-      environment: 'jsdom',
-      exclude: [
-        '**/node_modules/**',
-        '**/dist/**',
-        '**/cypress/**',
-        '**/.{idea,git,cache,output,temp}/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-        './e2e/**'
-      ],
-      globals: true,
-      transformMode: { web: [/\.[jt]sx?$/] },
-      setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect.js'],
-      // otherwise, solid would be loaded twice:
-      deps: { registerNodeLoader: true },
-      // if you have few tests, try commenting one
-      // or both out to improve performance:
-      threads: false,
-      isolate: false
     }
   }
 })
